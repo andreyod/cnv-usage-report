2a375dad2b0e6db20d9b2635077340a94c4184d1
Andrey Odarenko <andreyo@il.ibm.com>
2021-02-01 17:20:49 +0200

    Change query

--- report.py
+++ report.py
@@ -22,8 +22,8 @@ def weekly_email_domains(pc, end_time):
     q = """count by (email_domain)((subscription_sync_total{installed=~"kubevirt-hyperconverged.*"}) + on (_id) group_left(email_domain)( 0 * topk by (_id) (1, subscription_labels{email_domain!~"redhat.com|.*ibm.com"})))"""
     v = pc.custom_query_range(
             query=q,
-            start_time=end_time - datetime.timedelta(days=6),
-            end_time=end_time, step='1d')
+            start_time=end_time - datetime.timedelta(days=7),
+            end_time=end_time, step='1h')
     return frozenset(map(lambda x: x['metric']['email_domain'], v))
 
 def get_percentage(part, whole):
@@ -38,11 +38,7 @@ def running_vms(pc, end_time):
             end_time=end_time, step='1h')
     current = v[-1]['values'][-1][-1]
     last_week = v[-1]['values'][0][-1]
-    print(current, "VMs running externally (compared to", v[-1]['values'][0][-1], "last week).")
-    if current > last_week:
-        print(get_percentage(float(current)-float(last_week), float(last_week)), "%", "increase from last week.")
-    else:
-        print(get_percentage(float(last_week)-float(current), float(last_week)), "%", "decrease from last week.")
+    return "(running " + current + " VMs in total, compared to " + last_week + " last week):"
 
 def csvs_by_version(pc, end_time):
     end_time = datetime.datetime.now()
@@ -58,11 +54,11 @@ def csvs_by_version(pc, end_time):
     for i, v in enumerate(sorted_csvs):
         if i != 0:
             print_string = print_string + "; "
-        print_string = print_string+v[0]+"("+str(v[1])+") - "+str(get_percentage(v[1], total))+"% "
+        print_string = print_string+v[0]+"("+str(v[1])+", "+str(round(float(get_percentage(v[1], total))))+"%"+")"
     return print_string
 
 def vms_by_domain(pc, end_time, all_domains):
-    q = """sort_desc(sum by (email_domain)(cnv:vmi_status_running:count + on (_id) group_left(_blah) (0 * group by (_id, email_domain) (id_version_ebs_account_internal:cluster_subscribed{internal=""})) + on (_id) group_left(email_domain)( 0 * group by (_id,email_domain) (subscription_labels))))"""
+    q = """sort_desc(sum by (email_domain)(cnv:vmi_status_running:count + on (_id) group_left(_blah) (0 * group by (_id) (id_version_ebs_account_internal:cluster_subscribed{internal=""}))+ on (_id) group_left(email_domain) (0 * topk by (_id) (1, subscription_labels{}))))"""
     v = pc.custom_query_range(
             query=q,
             start_time=end_time - datetime.timedelta(hours=3),
@@ -75,17 +71,16 @@ def vms_by_domain(pc, end_time, all_domains):
     for i, v in enumerate(domains_with_vms):
         if i != 0:
             print_string = print_string + "; "
-        print_string = print_string + v[0] + "(" + str(v[1]) + " VMs)"
+        print_string = print_string + v[0] + "(" + str(v[1]) + ")"
     for v in domains_without_vms:
-        print_string = print_string + "; " + v + "(0 VMs)"
+        print_string = print_string + "; " + v
     return print_string
 
 
 now = datetime.datetime.now()
-running_vms(pc, now)
-print("\nNumber of OLM CSVs by version:\n", csvs_by_version(pc, now))
+print("Number of OLM CSVs by version:\n", csvs_by_version(pc, now))
 recent = weekly_email_domains(pc, now)
-print("\nRecent week had {} domains:".format(len(recent)))
+print("\nRecent week had {} domains".format(len(recent)), running_vms(pc, now))
 print(vms_by_domain(pc, now, recent), "\n")
 previous = weekly_email_domains(pc, now - datetime.timedelta(days=7))
 print("Added:", ' '.join(sorted(recent - previous)), "\n")
